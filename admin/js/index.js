c6fb0a129a4086714ea905cf2b6c8c48f366bcc5
Thiago Moreira <roxbhz@gmail.com>
2024-05-13 19:50:19 -0300

    New

--- admin/js/index.js
+++ admin/js/index.js
@@ -2,3 +2,14 @@ function openos() {
     window.open("newos.html");
 }
 
+   // Função para abrir o pop-up
+   function openPopup() {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+// Função para fechar o pop-up
+function closePopup() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+}
